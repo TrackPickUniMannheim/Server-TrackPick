74548614e493bfd340dd888f5263b143db3ec827
Niranjan Basnet <nbasnet@mail.uni-mannheim.de>
2017-03-22 21:18:34 +0100

    Added new file
    Dispatcher handler added.

--- Server-TrackPick/serverTCPReceiver.py
+++ Server-TrackPick/serverTCPReceiver.py
@@ -17,7 +17,7 @@ Instantiated once per connection to the server, and must override the handle() m
         self.request.sendall(self.data.upper())
 
 if __name__ == "__main__":
-    HOST, PORT = "0.0.0.0", 9999 #Handler for the port and IP
+    HOST, PORT = "0.0.0.0", 9999 #Handler for the port and IP with
 
     # Create the server, binding to localhost on port 9999
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
