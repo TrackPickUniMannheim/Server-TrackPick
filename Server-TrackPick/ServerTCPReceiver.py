cd5dc6afb2bb8b8f0dbeceee9c083ce2ec2a9593
ZuliWu <zuwu@mail.uni-mannheim.de>
2017-03-25 14:42:00 +0100

    change base handler
    change base handler  to stream handler

--- Server-TrackPick/ServerTCPReceiver.py
+++ Server-TrackPick/ServerTCPReceiver.py
@@ -1,30 +1,23 @@
+
 import socketserver
-import socket
-
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    """
-The request handler class for server.
-Instantiated once per connection to the server, and must override the handle() method to implement communication to the client.
-"""
-
-while(True):
-
+class MyTCPHandler(socketserver.StreamRequestHandler):
+    
     def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
+        # self.rfile is a file-like object created by the handler;
+        # we can now use e.g. readline() instead of raw recv() calls
+        self.data = self.rfile.readline().strip()
         print("{} wrote:".format(self.client_address[0]))
         print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        # Likewise, self.wfile is a file-like object used to write back
+        # to the client
+        self.wfile.write(self.data.upper())
+
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999 #Handler for the port and IP with
-
+    HOST, PORT = "localhost", 9999
+    
     # Create the server, binding to localhost on port 9999
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
-    #server.bind(("0.0.0.0", 9999))
-
+    
     # Activate the server
     server.serve_forever()
-
-
