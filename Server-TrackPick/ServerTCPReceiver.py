d5eb85fc8956aac9c2d8aad76a8ac6f890ae6555
Niranjan Basnet <nbasnet@mail.uni-mannheim.de>
2017-04-03 15:19:56 +0200

    Added timestamp
    Timestamp in miliseconds added. But explicitly.

--- Server-TrackPick/ServerTCPReceiver.py
+++ Server-TrackPick/ServerTCPReceiver.py
@@ -1,21 +1,22 @@
 from socketserver import ThreadingMixIn,ForkingMixIn,StreamRequestHandler
 import socket,select
+import time
 
-s = socket.socket()
-#host = socket.gethostname()
+TCPInSocket = socket.socket()
 host = '127.0.0.1'
 print(host)
 port = 9999
-s.bind((host,port))
-s.listen(5)
-inputs = [s]
+TCPInSocket.bind((host,port))
+TCPInSocket.listen(5)
+inputs = [TCPInSocket]
 while True:
     rs,ws,es = select.select(inputs,[],[])
+    servertime = int(round(time.time()*1000))
     for r in rs:
-        if r is s:
-            c,addr = s.accept()
+        if r is TCPInSocket:
+            c,addr = TCPInSocket.accept()
             inputs.append(c)
-            print(addr)
+            print((str(addr))+"Server"+(str(servertime)))
         else:
             try:
                 data = r.recv(1024)
