d32dc0bb7002075b56d1e1bd0fa2057f83cafb80
Niranjan Basnet <nbasnet@mail.uni-mannheim.de>
2017-06-21 16:22:21 +0200

    Completion of Pipeline
    Pipeline is completed with data insertion working. Further for testing.

--- Server-TrackPick/DatabaseController.py
+++ Server-TrackPick/DatabaseController.py
@@ -2,189 +2,41 @@ import pika
 import sys
 import time
 
+#@Developers: Niranjan Basnet, Zuli Wu
 
-#Device and sensor definitions
-Device1 = 'F909023'
-Sensor11 = 'Acceleromter'
-Sensor12 = 'Magnetometer'
-Sensor13 = 'Gyroscope'
-Document11 = Device1+Sensor11
-Document12 = Device1+Sensor12
-Document13 = Device1+Sensor13
-
-Device2 = 'F909022'
-Sensor21 = 'Acceleromter'
-Sensor22 = 'Magnetometer'
-Sensor23 = 'Gyroscope'
-Document21 = Device1+Sensor21
-Document22 = Device1+Sensor22
-Document23 = Device1+Sensor23
-
-Device3 = 'F909021'
-Sensor31 = 'Acceleromter'
-Sensor32 = 'Magnetometer'
-Sensor33 = 'Gyroscope'
-Document31 = Device1+Sensor31
-Document32 = Device1+Sensor32
-Document33 = Device1+Sensor33
-
+# Expected Stream format: "{"deviceid":"d37825daa97041dd","sensortype":"accelerometer","
+# data":[{"timestamp":"1496078282698","x":"2.23517e-7","y":"9.77631","z":"0.812348"}]}
 
 userid = " ".join(sys.argv[1:]) # Userid from the terminal signifies the collection name for the user
 datenow = time.strftime("%d/%m/%Y-%X") # Date time signifies the unique timestamp recorded in session
 datenow = str(datenow)
 collname = userid+'-'+datenow # Collection name when new instance is created
 
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client['test_database']
-collection = db['test_database'] # test_database to mock up and test instead of the actual database 'trackpick'
+try:
 
-if collection is not None: # See whether the instance of database is created or not
+    from pymongo import MongoClient
+    client = MongoClient('localhost', 27017)
+    db = client['test_database']
+    collection = db['test_database'] # test_database to mock up and test instead of the actual database 'trackpick'
 
-    print('Database Instance Present')
+    if collection is not None: # See whether the instance of database is created or not
 
-db.create_collection(collname)
-getcoll = db.get_collection(collname)
-getcoll = str(getcoll)
-getcoll = getcoll[129:-2]
-if getcoll == collname: # See whether collection is created or not
-    print('Collection with name '+ collname + 'successfully created')
-    # Creating Documents for First Device here
-    if Device1 == 'F909023' and Sensor11 == 'Acceleromter':
-        db.get_collection(name=getcoll).insert_one(
-            {
-                "deviceid": Device1,    # Device 1 with first sensor and so on
-                "sensor": Sensor11,
-                "values":[
-                    {
+        print('Database Instance Present')
 
-                    }
-                ]
-            }
-        )
-        print("Document11 Created")
-    if Device1 == 'F909023' and Sensor12 == 'Magnetometer':
-        db.get_collection(name=getcoll).insert_one(
-        {
-                "deviceid": Device1,
-                "sensor": Sensor12,
-                "values": [
-                    {
-
-                     }
-                    ]
-        }
-        )
-        print("Document12 Created")
-
-    if Device1 == 'F909023' and Sensor13 == 'Gyroscope':
-        db.get_collection(name=getcoll).insert_one(
-        {
-                "deviceid": Device1,
-                "sensor": Sensor13,
-                "values": [
-                    {
-
-                     }
-                    ]
-        }
-        )
-
-        print("Document13 Created")
-    # Creating documents for second device
-    if Device2 == 'F909022' and Sensor21 == 'Acceleromter':
-        db.get_collection(name=getcoll).insert_one(
-            {
-                "deviceid": Device2,    # Device 1 with first sensor and so on
-                "sensor": Sensor21,
-                "values":[
-                    {
-
-                    }
-                ]
-            }
-        )
-        print("Document21 Created")
-    if Device2 == 'F909022' and Sensor22 == 'Magnetometer':
-        db.get_collection(name=getcoll).insert_one(
-        {
-                "deviceid": Device2,
-                "sensor": Sensor22,
-                "values": [
-                    {
-
-                     }
-                    ]
-        }
-        )
-        print("Document22 Created")
-
-    if Device2 == 'F909022' and Sensor23 == 'Gyroscope':
-        db.get_collection(name=getcoll).insert_one(
-        {
-                "deviceid": Device2,
-                "sensor": Sensor23,
-                "values": [
-                    {
-
-                     }
-                    ]
-        }
-        )
-
-        print("Document23 Created")
-    #Creating documents for third device
-
-    if Device3 == 'F909021' and Sensor31 == 'Acceleromter':
-        db.get_collection(name=getcoll).insert_one(
-            {
-                "deviceid": Device3,    # Device 1 with first sensor and so on
-                "sensor": Sensor31,
-                "values":[
-                    {
-
-                    }
-                ]
-            }
-        )
-        print("Document31 Created")
-    if Device3 == 'F909021' and Sensor32 == 'Magnetometer':
-        db.get_collection(name=getcoll).insert_one(
-        {
-                "deviceid": Device3,
-                "sensor": Sensor32,
-                "values": [
-                    {
-
-                     }
-                    ]
-        }
-        )
-        print("Document32 Created")
-
-    if Device3 == 'F909021' and Sensor33 == 'Gyroscope':
-        db.get_collection(name=getcoll).insert_one(
-        {
-                "deviceid": Device3,
-                "sensor": Sensor33,
-                "values": [
-                    {
-
-                     }
-                    ]
-        }
-        )
-
-        print("Document33 Created")
-
-
-
-else:
-    print('Collection not created')
+    db.create_collection(collname)
+    getcoll = db.get_collection(collname)
+    getcoll = str(getcoll)
+    getcoll = getcoll[129:-2]
+    if getcoll == collname: # See whether collection is created or not
+        print('Collection with name '+ collname + 'successfully created')
+    else:
+        print('Collection not created')
+except:
+    print('Database not found')
 
 #Start with connection with queue and fetch the data
 
-print ('Fetching data...')
+print ('Fetching data and inserting into database...')
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')) # Connection with the designated queue
 channel = connection.channel()
@@ -195,15 +47,26 @@ def callback(ch, method, properties, indata):
     if indata is None:
         print("No data in the queue")
     else:
-        print("Queue name is %r" % indata)
+        indata = indata.decode('utf-8')
+        db.get_collection(name=getcoll).insert_one(
+            {
+                "finaldata":[indata]
 
+           }
+        )
+        print("Insertion Complete...")
 
-channel.basic_consume(callback,
-                      queue='trackPick')
+channel.basic_consume(callback, queue='trackPick')
+
 
 channel.queue_declare(exclusive=True)
 
 channel.start_consuming()
+
+
+
+#Start fetching the data from the queue
+#print(channel)
 
 connection.close()
 
--- Server-TrackPick/ServerTCPReceiver.py
+++ Server-TrackPick/ServerTCPReceiver.py
@@ -3,6 +3,9 @@ import socket
 import threading
 import time
 import pika
+import subprocess
+
+#@Developers: Niranjan Basnet, Zuli Wu
 
 QUIT = False
 
@@ -21,37 +24,39 @@ class ClientThread(threading.Thread): # Class that implements the client threads
         done = False
         data = self.readline() # Read data from the socket and process it
 
-        if data is 'b%':
+        if data is "b''%":
             self.writeline("No data thus quitting")
             QUIT = True
             done = True
 
         while not done:
-            #if 'quit' == data:
-                #self.writeline('Quitting...')
-                #QUIT = True
-                #done = True
+
             # Case where data was received
             if data is None:
                 self.client.close()
             else:
-                arrivetime = int(round(time.time() * 1000)) #Arrival time taken on each thread and concatinated with the data
-                data = str(data) + "Arrival Time" + str(arrivetime)
-                #data = data.encode('utf-8')
-                print(data) # Main data for Incoming Data
+                servertime = int(round(time.time() * 1000))  # Arrival time taken on each
+                                                                # thread and concatinated with the data
+                # Wrapping with external JSON for server time
+                data = data.decode('utf-8')
+                outdata = '{"servertime":' + '"' + str(servertime) + '","cdata":[' + str(data) + ']}'
+                print(outdata)
                 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost')) # connect via pika with localhost
-                channel = connection.channel() # connect channel through connection
+                channel = connection.channel() # Connect channel through connection
 
                 channel.queue_declare(queue='trackPick') # Queue declaration with "trackPick"
                 channel.basic_publish(exchange='',
                                       routing_key='trackPick',  # Routing with key "trackPick"
-                                      body=data)
+                                      body=outdata)
+
+                print("Sending to queue...")
 
                 if data is None:
                     QUIT = True
                     done = True
 
-                print("Still Sending data...")
+
+                print("Server not yet closed...")
                 connection.close()
 
 
@@ -108,7 +113,8 @@ class Server:
                 break
             except socket.error: # Handling Binding Error
 
-                print('Socket connection error... Waiting 5 seconds to retry.') # Short 5 seconds time for socket replinishment
+                print('Socket connection error... Waiting 5 seconds to retry.') # Short 5 seconds
+                                                                                # time for socket replinishment
 
                 del self.sock
                 time.sleep(5)
